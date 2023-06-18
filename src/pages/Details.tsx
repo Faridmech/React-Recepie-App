c7a786c81d9c82f35dd11d5a2d16b9c8b2c4e779
Faridmech <feridh189@gmail.com>
2023-06-18 19:21:41 +0400

    Finished

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,4 +1,14 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  ListItem,
+  Tabs,
+  UnorderedList,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Image } from "@chakra-ui/react";
@@ -13,6 +23,8 @@ type TDetails = {
   strIngredient4: string;
   strIngredient5: string;
   strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
 };
 export const Details: React.FC = () => {
   const { id } = useParams();
@@ -52,18 +64,86 @@ export const Details: React.FC = () => {
           <Box>
             <Image src={details?.strMealThumb} alt="" w="500px" h="450px" />
           </Box>
-          <Box >
-            <Box display="flex" flexDirection="row" gap="2rem" marginLeft='3rem'>
-              <Box>
-                <Text fontSize='2rem' border='2px solid black' width='11rem' textAlign='center'>Instruction</Text>
+          <Box marginLeft='2rem'>
+            <Tabs>
+              <Box marginLeft='2rem' gap='4rem'>
+              <TabList>
+                <Tab>Instruction</Tab>
+                <Tab>Ingridinet</Tab>
+              </TabList>
               </Box>
-              <Box>
-                <Text fontSize='2rem' border='2px solid black' width='11rem' textAlign='center'>Ingridient</Text>
-              </Box>
-            </Box>
-            <Box paddingLeft="0.5rem">
-              <p>{details?.strInstructions}</p>
-            </Box>
+              <TabPanels>
+                <TabPanel>
+                  <Box paddingLeft="0.5rem">
+                    <p>{details?.strInstructions}</p>
+                  </Box>
+                </TabPanel>
+                <TabPanel>
+                <Box marginLeft="1rem">
+                  <UnorderedList>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient1}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {" "}
+                      {details?.strIngredient2}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient3}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient4}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient5}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient6}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {" "}
+                      {details?.strIngredient7}
+                    </ListItem>
+                    <ListItem
+                      listStyleType="none"
+                      fontSize="1.5rem"
+                      color="brown"
+                    >
+                      {details?.strIngredient8}
+                    </ListItem>
+                  </UnorderedList>
+                </Box>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </Box>
         </Box>
       </Box>
